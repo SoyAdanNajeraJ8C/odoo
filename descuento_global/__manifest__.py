a4790b6f67cef5959fc01c28814a6a3911acdfa3
Adan <adanasr@gmail.com>
2022-03-25 11:53:24 -0600

    Nuevos modulos

--- descuento_global/__manifest__.py
+++ descuento_global/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    'name': 'Descuento Global',
-    'version': '02.06.2021',
-    'summary': """Descuento Global""",
-    'description': 'Descuento Global en Ventas by Adan Najera',
+    'name': 'Global Discount',
+    'version': '18.06.2021',
+    'summary': """Global Discount""",
+    'description': 'Sales Global Discount',
     "category": "Sale",
-    "author": "Soy Adan Najera",
-    'depends': ['sale_margin'],
+    "author": "J8C",
+    'website': 'https://juarezsoft.com.mx/',
+    'depends': ['sale'],
     'data': [
         'views/sale_views.xml',
     ],
--- descuento_global/models/orderline_global_discount.py
+++ descuento_global/models/orderline_global_discount.py
@@ -17,33 +17,8 @@ from odoo.tools.misc import formatLang, get_lang
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_descuento_1 = fields.Integer("Descuento General(%)")
-    x_descuento_value = fields.Integer(string="Descuento General ($)",readonly=True)
-    representate_legal = fields.Many2one('res.partner', string='Representante Legal')
-    # tipo_cambio = fields.Float(string='Tipo de Cambio')
-
-
-    # @api.depends('order_line.price_total')
-    # def _amount_all(self):
-    #     """
-    #     Compute the total amounts of the SO.
-    #     """
-    #     for order in self:
-    #         descuento_value = order.x_descuento_1
-    #         amount_untaxed = amount_tax = 0.0
-    #         for line in order.order_line:
-    #             line.discount = descuento_value
-    #         for line in order.order_line:
-    #             amount_untaxed += line.price_subtotal
-    #             amount_tax += line.price_tax
-    #         # descuento = (amount_untaxed * descuento_value) / 100
-    #         order.update({
-    #             # 'x_descuento_value': descuento,
-    #             'amount_untaxed': amount_untaxed,
-    #             'amount_tax': amount_tax,
-    #             # 'amount_total': (amount_untaxed-descuento) + amount_tax,
-    #             'amount_total': amount_untaxed + amount_tax,
-    #         })
+    x_descuento_1 = fields.Integer("Global Discount (%)")
+    x_descuento_value = fields.Integer(string="Global Discount ($)",readonly=True)
 
     def discount_global(self):
          for order in self:
@@ -54,12 +29,9 @@ class SaleOrder(models.Model):
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
-            # descuento = (amount_untaxed * descuento_value) / 100
             order.update({
-                # 'x_descuento_value': descuento,
                 'amount_untaxed': amount_untaxed,
                 'amount_tax': amount_tax,
-                # 'amount_total': (amount_untaxed-descuento) + amount_tax,
                 'amount_total': amount_untaxed + amount_tax,
             })
 
