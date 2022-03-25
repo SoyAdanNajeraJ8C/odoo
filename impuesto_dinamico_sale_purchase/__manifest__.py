51bde3744dc3b283a5b92b998217b2a3798e94fa
Adan <adanasr@gmail.com>
2022-03-25 12:01:19 -0600

    Manifest

--- impuesto_dinamico_sale_purchase/__manifest__.py
+++ impuesto_dinamico_sale_purchase/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'Default Taxes',
     'version': '15.02.2022',
-    'summary': """Default Taxes by Adan J8C""",
-    'description': 'Default Taxes by Adan J8C',
+    'summary': """Default Taxes by J8C""",
+    'description': 'Default Taxes by J8C',
     "category": "Sale",
     "author": "Soy Adan Najera J8C",
     'depends': ['sale', 'base'],
